e1968658e8835178be854b0532810536c3c4b89f
AVAVT <itsavavt@gmail.com>
2016-12-07 21:43:26 +0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var Nakama = {};
+Nakama.configs = {
+}
+
+window.onload = function(){
+  Nakama.game = new Phaser.Game(640,960,Phaser.AUTO,'',
+    {
+      preload: preload,
+      create: create,
+      update: update,
+      render: render
+    }, false, false
+  );
+}
+
+// preparations before game starts
+var preload = function(){
+  Nakama.game.scale.minWidth = 320;
+  Nakama.game.scale.minHeight = 480;
+  Nakama.game.scale.maxWidth = 640;
+  Nakama.game.scale.maxHeight = 960;
+  Nakama.game.scale.pageAlignHorizontally = true;
+  Nakama.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+  Nakama.game.time.advancedTiming = true;
+}
+
+// initialize the game
+var create = function(){
+  Nakama.game.physics.startSystem(Phaser.Physics.ARCADE);
+  Nakama.keyboard = Nakama.game.input.keyboard; 
+}
+
+// update game state each frame
+var update = function(){}
+
+// before camera render (mostly for debug)
+var render = function(){}
