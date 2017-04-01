d1a88a8f9585d525d75add77cad4ec715e6015df
T-Rekt <t.to.the.rekt@gmail.com>
2017-04-02 04:19:09 +0700

    Removed RailController

--- js/RailController.js
+++ js/RailController.js
@@ -1,34 +1,53 @@
-class RailController{
-	constructor(position) {
+// class RailController{
+// 	constructor(position) {
+//     var parts = [];
 
-	}
-}
+//     RailController.colors.forEach(function(color) {
+//       let sprite = new ObstacleController("circle_"+color,color,RailController.configs,RailController.configs.offsetAngle,position);
+//       parts.push(sprite.sprite);
+//     });
+// 	}
+// }
 
-/*class CircleController{
-  constructor(position) {
-    var parts = [];
+// RailController.colors= [
+//   {color:"cyan",position: 0},
+//   {color:"pink",position: {y:position.y,x:RailController.configs.width}},
+//   {color:"purple",position: {y:position.y,x:2*RailController.configs.width}},
+//   {color:"yellow",position: {y:position.y,x:3*RailController.configs.width}}
+// ];
 
-    CircleController.colors.forEach(function(color) {
-      let sprite = new ObstacleController("circle_"+color,color,CircleController.configs,CircleController.configs.offsetAngle,position);
-      parts.push(sprite.sprite);
-    });
+// RailController.configs= {
+//   anchor:{x:0.5,y:0.5},
+//   ratio:1,
+//   offsetAngle: 0,
+//   width:144,
+//   height:32
+// }
+// class RailController{
+//   constructor(position) {
+//     var parts = [];
 
-    this.parts = parts;
-    this.position = position;
-  }
+//     RailController.colors.forEach(function(color) {
+//       let sprite = new ObstacleController("circle_"+color,color,RailController.configs,RailController.configs.offsetAngle,position);
+//       parts.push(sprite.sprite);
+//     });
 
-  update() {
-    this.parts.forEach(function(part) {
-      part.body.clearShapes();
-      if (part.color!=CSW.player.sprite.color) part.body.loadPolygon('circle_physics', part.color);
-    });
-  }
-}
+//     this.parts = parts;
+//     this.position = position;
+//   }
 
-CircleController.configs= {
-  anchor:{x:0.5,y:0.5},
-  ratio:0.5,
-  offsetAngle: 0
-}
+//   update() {
+//     this.parts.forEach(function(part) {
+//       part.body.clearShapes();
+//       if (part.color!=CSW.player.sprite.color) part.body.loadPolygon('circle_physics', part.color);
+//     });
+//   }
+// }
 
-CircleController.colors= ["cyan","pink","purple","yellow"];*/
+// RailController.configs= {
+//   anchor:{x:0.5,y:0.5},
+//   ratio:0.5,
+//   offsetAngle: 0
+// }
+
+// RailController.colors= ["cyan","pink","purple","yellow"];
